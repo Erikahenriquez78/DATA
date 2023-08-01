77a0cf8ce686b1af53a8932bb473f801deb43da0
jpucholv <96960258+jpucholv@users.noreply.github.com>
2023-08-01 17:23:12 +0200

    v1_filtro

--- app/src/data_processing.py
+++ app/src/data_processing.py
@@ -4,8 +4,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import ast
+import math
 
-def v2_intensidad_process(intensidad):
+def haversine_distance(lat1, lon1, lat2, lon2):
+    r = 6371  # Radio medio de la Tierra en kilómetros
+
+    # Convertir las latitudes y longitudes de grados a radianes
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    # Calcular las diferencias de latitud y longitud
+    d_lat = lat2_rad - lat1_rad
+    d_lon = lon2_rad - lon1_rad
+
+    # Aplicar la fórmula de Haversine
+    a = math.sin(d_lat/2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(d_lon/2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = r * c
+
+    return distance
+
+def v1_intensidad_process(intensidad):
     if intensidad == 0:
         return 2.0
     elif intensidad == 1:
@@ -17,15 +38,20 @@ def v2_intensidad_process(intensidad):
         
 
 def sim_mat_plot(similarity):
-    sns.heatmap(similarity, cmap='coolwarm', center=0)
+    sns.heatmap(similarity, center=0)
     return plt.show()
 
     
 def v1_query_process(edad, sexo, peso, condicion, objetivo, preferencias, posicion,
                   distancia, clima, temperatura, humedad, deportes_normalized, deportes, items):
     
+    # Convertir las preferencias en una lista de deportes
     preferencias = ast.literal_eval(preferencias)
     
+    # Convertir la posición del usuario a una lista de números
+    posicion = ast.literal_eval(posicion)
+    latitud_usuario, longitud_usuario = float(posicion[0]), float(posicion[1])
+
     # Convertir el diccionario en un DataFrame sin índice específico
     perfil_usuario = deportes[deportes['Actividad'].isin(preferencias)].copy()
     
@@ -48,8 +74,23 @@ def v1_query_process(edad, sexo, peso, condicion, objetivo, preferencias, posici
         if similarity_scores[0][indice] > umbral_similitud:
             recomendaciones.append(deportes['Actividad'].iloc[indice])
 
-    return {"Deportes recomendados para el usuario": recomendaciones}
+    # Convertir la posición del usuario a números
+    latitud_usuario = float(posicion[0])
+    longitud_usuario = float(posicion[1])
+
+    # Filtrar los items que contengan alguna de las recomendaciones en su campo 'EQUIPAMIENTO'
+    items_filtrados = items[items['EQUIPAMIENTO'].str.contains('|'.join(recomendaciones))].to_dict(orient='records')
+
+    # Calcular la distancia a cada uno de los items en función de la posición del usuario
+    for item in items_filtrados:
+        item['DISTANCIA'] = haversine_distance(latitud_usuario, longitud_usuario, item['LATITUD'], item['LONGITUD'])
+
+    # Filtrar los items_filtrados con el valor de distancia
+    items_filtrados = [item for item in items_filtrados if item['DISTANCIA'] <= distancia]
+
+    return {"Deportes recomendados para el usuario": recomendaciones, "Items filtrados": items_filtrados}
 
 
 def v2_query_process(edad, sexo, peso, condicion, objetivo, preferencias,
                      posicion, distancia, clima, temperatura, humedad):
+    pass
